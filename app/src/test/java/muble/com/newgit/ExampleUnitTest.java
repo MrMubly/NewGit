66b072d0a08dadddc2009bc5e5e31f04e213cc25
MrMuble <690617264@qq.com>
2016-06-30 15:58:02 +0800

    爷们第一次，不要失败哦

---
+++ app/src/test/java/muble/com/newgit/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package muble.com.newgit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
