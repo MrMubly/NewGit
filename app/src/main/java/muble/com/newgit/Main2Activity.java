19cc35ee1fffb08675bde6e395b6b069e464be7d
MrMuble <690617264@qq.com>
2016-06-30 16:35:04 +0800

    爷们第一次，不要失败哦

---
+++ app/src/main/java/muble/com/newgit/Main2Activity.java
@@ -0,0 +1,14 @@
+package muble.com.newgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
